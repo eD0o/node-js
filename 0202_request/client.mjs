d9157eaedd51d447e1d8a250edf297e972529cc6
Eduardo <eduardo.benigno15@gmail.com>
2025-12-10 06:44:36 -0300

    feat: preparing class 0301

--- 0202_request/client.mjs
+++
@@ -1,8 +0,0 @@
-const res = await fetch("http://localhost:3000/products?color=blue&size=g", {
-  method: "POST",
-  headers: { 'Content-Type': 'application/json' },
-});
-
-const body = await res.text();
-
-console.log(body)
